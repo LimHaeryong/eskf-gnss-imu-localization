d4006513b61e6549e391b912432a960a73a80b48
LimHaeryong <105035533+LimHaeryong@users.noreply.github.com>
2023-10-12 18:48:00 +0900

    Remove unnecessary comment

--- modules/opengl_viewer/include/opengl_viewer/shader.h
+++ modules/opengl_viewer/include/opengl_viewer/shader.h
@@ -6,7 +6,6 @@
 class Shader
 {
 public:
-    // 객체를 생성하려면 createFromFile 함수를 이용할 수 밖에 없음.
     static std::unique_ptr<Shader> createFromFile(const std::string& filename, GLenum shaderType);
     ~Shader();
     uint32_t get() const {return mShader;}
